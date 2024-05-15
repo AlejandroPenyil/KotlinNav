12f96cb6315c3aa3a4cab934b0fe34740952803f
apenil <alejandro.penil@gmail.com>
2024-05-15 16:10:28 +0200

    darkmode saved but not loaded since enter in config

--- app/src/main/java/com/example/nav/ui/ThirdFragment.kt
+++ app/src/main/java/com/example/nav/ui/ThirdFragment.kt
@@ -8,9 +8,13 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.nav.databinding.FragmentThirdBinding
+import kotlinx.coroutines.launch
 
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
@@ -30,6 +34,7 @@ class ThirdFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeDarkMode()
         initUI()
     }
 
@@ -46,11 +51,31 @@ class ThirdFragment : Fragment() {
             AppCompatDelegate.MODE_NIGHT_NO
         }
         AppCompatDelegate.setDefaultNightMode(currentMode)
+
+        // Guardar el estado del modo oscuro
+        lifecycleScope.launch {
+            requireContext().dataStore.edit { preferences ->
+                preferences[IS_DARK_MODE_KEY] = isDarkMode
+            }
+        }
     }
 
+    private fun observeDarkMode() {
+        lifecycleScope.launchWhenStarted {
+            requireContext().dataStore.data.collect { preferences ->
+                val isDarkMode = preferences[IS_DARK_MODE_KEY] ?: false
+                binding.switchDarkMode.isChecked = isDarkMode
+                applyDarkMode(isDarkMode)
+            }
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private val IS_DARK_MODE_KEY = booleanPreferencesKey(("is_dark_mode"))
     }
 }
