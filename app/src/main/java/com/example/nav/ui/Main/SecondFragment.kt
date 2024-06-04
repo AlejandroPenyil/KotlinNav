828ecda7d5607d49ad98f910500916b2b9580600
apenil <alejandro.penil@gmail.com>
2024-06-04 13:11:14 +0200

    Trying to nav between freagments with buttons

--- app/src/main/java/com/example/nav/ui/Main/SecondFragment.kt
+++ app/src/main/java/com/example/nav/ui/Main/SecondFragment.kt
@@ -1,7 +1,39 @@
 package com.example.nav.ui.Main
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.nav.R
+import com.example.nav.databinding.FragmentFirstBinding
+import com.example.nav.databinding.FragmentSecondBinding
+import com.example.nav.databinding.FragmentSolicitudPresupuestoBinding
+import com.example.nav.ui.Main.Presupuesto.SolicitudPresupuestoFragment
+import com.example.nav.ui.Start.Start
 
 class SecondFragment:Fragment(R.layout.fragment_second) {
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        binding.SolPreBTN.setOnClickListener{ goToSolicitud() }
+    }
+
+    private fun goToSolicitud() {
+    }
 }
